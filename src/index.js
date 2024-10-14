5f71b4abc2c6f48c590630ec26a559e58ec8408e
marjorymell <marjorymel48@gmail.com>
2024-10-14 14:51:31 -0300

    cleanup: remove comment

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const Turma = require('./Turma')
 const TurmaPresencial = require('./TurmaPresencial')
 
 
-const aluno1 = new Aluno('QA', 'teste123', '12345678');
+const aluno1 = new Aluno('QA', 'teste123', '12345678')
 console.log(aluno1)
 
 const turma1 = new Turma('TURMATest', 8.5)
 console.log(`Turma: ${turma1.aprovado()}`)
 
-const turmaPresencial1 = new TurmaPresencial('TURMAPresencial', 7.5, 80);
+const turmaPresencial1 = new TurmaPresencial('TURMAPresencial', 7.5, 80)
 console.log(`Turma Presencial: ${turmaPresencial1.aprovado()}`)
